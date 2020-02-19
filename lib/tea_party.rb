40cc7e2c3002da6757c32ae0ffe21d72912e4500
Tylor Schafer <tylorschafer@gmail.com>
2020-02-18 21:20:17 -0700

    Add tea_party kata

--- lib/tea_party.rb
+++ lib/tea_party.rb
@@ -5,4 +5,11 @@
 # The return string should be "Hello (title) (last_name)
 
 def welcome(last_name, is_woman, is_sir)
+  if is_sir
+    "Hello Sir #{last_name}"
+  elsif is_woman
+    "Hello Ms. #{last_name}"
+  else
+    "Hello Mr. #{last_name}"
+  end
 end
